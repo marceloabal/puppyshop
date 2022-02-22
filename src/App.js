5d5b70c42f5bdc6984c94e8390b23a4bb12c9d2a
marceloabal <marcelo.abal@gmail.com>
2022-02-22 16:11:29 -0300

    Menu Bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import MainMenu from './components/MainMenu';
+
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
+        <p>
+        <MainMenu />
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>Puppy Shop</h1>  
         </p>
         <a
           className="App-link"
